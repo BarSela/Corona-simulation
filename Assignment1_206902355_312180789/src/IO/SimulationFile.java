02fd245e7d11f0c743c4f12d3282baf333882966
Betsalel koginsky <Betsalelkogo@gmail.com>
2021-04-08 09:43:59 +0300

    file change

--- Assignment1_206902355_312180789/src/IO/SimulationFile.java
+++ Assignment1_206902355_312180789/src/IO/SimulationFile.java
@@ -1,5 +1,32 @@
 package IO;
 
-public class SimulationFile {
-
+public class SimulationFile
+{
+	public static File file= new File("data.txt");
+	public static void writeTofile()
+	{
+		try 
+		{
+			PrintWriter output=new PrintWriter(file);
+			output.println("City; Ashdod; 0;0; 90;50; 1000");
+			output.close();
+		}
+		catch (IOException ex) 
+		{
+			System.out.printf("Error %s\n",ex);
+		}
+	}
+	public static void readFromfile()
+	{
+		try 
+		{
+			Scanner in= new Scanner(file);
+			String name = in.nextLine();
+			System.out.printf("%s\n", name);
+		}
+		catch (FileNotFoundException ex) 
+		{
+			System.out.printf("Error %s\n",ex);
+		}
+	}
 }
