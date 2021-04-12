99cb65316ae0e7931571d03cdcf417bb7253eeee
Betsalel koginsky <Betsalelkogo@gmail.com>
2021-04-12 13:05:40 +0300

    fix the simulation betsalel is the 	KING

--- Assignment1_206902355_312180789/src/simulation/Main.java
+++ Assignment1_206902355_312180789/src/simulation/Main.java
@@ -26,10 +26,11 @@ public class Main {
 			 * step 2
 			 */
 			IVirus virus=new BritishVariant();
+			double[] numContagion=new double[Map.getSize()];//array for number of people that contagion in step 2
 			for(int i=0;i<Map.getSize();i++)
 			{
-				double numContagion=world.getSettlement()[i].getPopulation()*0.01;
-				for (int j=0;j<numContagion;j++)
+				numContagion[i]=world.getSettlement()[i].getPopulation()*0.01;
+				for (int j=0;j<numContagion[i];j++)
 				{
 					world.getSettlement()[i].getPeople().set(j,world.getSettlement()[i].getPeople().get(j).contagion(virus));
 				}
@@ -37,11 +38,11 @@ public class Main {
 			/**
 			 * step 3
 			 */
-			for (int i=0;i<numOfSimulation;i++)
+			for (int i=0;i<numOfSimulation;i++)//five simulation
 			{
-				for (int j=0;j<Map.getSize();j++)
+				for (int j=0;j<Map.getSize();j++)//run over all settlement
 				{	
-					for (int k=0;k<world.getSettlement()[j].getPopulation();k++)
+					for (int k=0;k<numContagion[j];k++)//
 					{
 						if (world.getSettlement()[j].getPeople().get(k) instanceof Sick)
 							for (int t=0;t<6;t++)
@@ -59,6 +60,10 @@ public class Main {
 					}
 				}
 			}
+			for (int i=0;i<Map.getSize();i++)
+				for(int j=0;j<world.getSettlement()[i].getPopulation();j++)
+					if (world.getSettlement()[i].getPeople().get(j) instanceof Sick)
+						System.out.println(world.getSettlement()[i].getPeople().get(j).toString());
 		}
 		catch (IOException e) 
 		{
