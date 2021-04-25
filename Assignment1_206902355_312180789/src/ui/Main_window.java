dbf2a80f2b012fc30290553b7a66326542936e36
Betsalel koginsky <Betsalelkogo@gmail.com>
2021-04-25 16:17:01 +0300

    WindowBuilder

--- Assignment1_206902355_312180789/src/ui/Main_window.java
+++ Assignment1_206902355_312180789/src/ui/Main_window.java
@@ -4,5 +4,9 @@ import javax.swing.JFrame;
 
 
 public class Main_window extends JFrame {
+	public Main_window() {
+		super("Corona-simulation");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
