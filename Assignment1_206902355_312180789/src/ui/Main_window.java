5406af78c657bd99b50db200709ad9581c494aef
Betsalel koginsky <Betsalelkogo@gmail.com>
2021-04-25 18:58:20 +0300

    add javadoc

--- Assignment1_206902355_312180789/src/ui/Main_window.java
+++ Assignment1_206902355_312180789/src/ui/Main_window.java
@@ -1,9 +1,14 @@
+/**
+ * @author Bar Sela            206902355
+ * @author Betsalel Koginsky   312180789
+ */
 package ui;
 
 import javax.swing.JFrame;
 
 
 public class Main_window extends JFrame {
+	
 	public Main_window() {
 		super("Corona-simulation");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
